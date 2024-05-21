60ee4ed7f6d5782bbdc8fe5a02c90f78846f3c43
Wonders Victor <Wondersprince1@gmail.com>
2024-05-21 09:14:22 +0100

    Working

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+'''LIFOCache Caching System'''
+from more_itertools import last
+from base_caching import BaseCaching
+
+
+class LIFOCache(BaseCaching):
+	'''LIFOCache Caching System'''
+	def put(self, key, item):
+		'''assigns to the dictionary self.cache_data the item value for the key key'''
+		if not key or not item:
+			return
+		if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+			last_key = last(self.cache_data)
+			self.cache_data.pop(last_key)
+			print(f"DISCARD: {last_key}")
+		self.cache_data[key] = item
+	
+	def get(self, key):
+		''' returns the value in self.cache_data linked to key.'''
+		if key:
+			if key not in self.cache_data.keys():
+				return
+			return self.cache_data[key]
