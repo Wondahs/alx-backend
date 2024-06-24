53e0273b79b5c452b10dc80d40cbcca521885c3d
Wonders Victor <Wondersprince1@gmail.com>
2024-06-24 23:34:13 +0100

    Kue

--- 0x03-queuing_system_in_js/1-redis_op.js
+++ 0x03-queuing_system_in_js/1-redis_op.js
@@ -1,31 +1,30 @@
 // task 2: Node Redis client and basic operations
-import 'redis';
 import { createClient, print } from 'redis';
 
-function setNewSchool(schoolName, value) {
-    client.set(`${schoolName}`, `${value}`, print);
+function setNewSchool (schoolName, value) {
+  client.set(`${schoolName}`, `${value}`, print);
 }
 
-function displaySchoolValue(schoolName) {
-    client.get(`${schoolName}`, (err, value) => {
-        if (err) {
-            console.log('Error getting key:', err);
-        } else {
-            console.log(value);
-        }
-    });
+function displaySchoolValue (schoolName) {
+  client.get(`${schoolName}`, (err, value) => {
+    if (err) {
+      console.log('Error getting key:', err);
+    } else {
+      console.log(value);
+    }
+  });
 }
 
 const client = createClient({
-    url: 'redis://localhost:6379'
+  url: 'redis://localhost:6379'
 });
 
 client.on('connect', () => {
-    console.log('Redis client connected to the server');
+  console.log('Redis client connected to the server');
 });
 
 client.on('error', (err) => {
-    console.log('Redis client not connected to the server:', err.toString());
+  console.log('Redis client not connected to the server:', err.toString());
 });
 
 displaySchoolValue('Holberton');
--- 0x03-queuing_system_in_js/5-subscriber.js
+++ 0x03-queuing_system_in_js/5-subscriber.js
@@ -1,6 +1,5 @@
-// task 1: Node Redis Client
+// Node Redis client subscriber
 import { createClient } from 'redis';
-
 const client = createClient({
   url: 'redis://localhost:6379'
 });
@@ -11,4 +10,14 @@ client.on('connect', () => {
 
 client.on('error', (err) => {
   console.log('Redis client not connected to the server:', err.toString());
+});
+
+client.subscribe('holberton school channel');
+
+client.on('message', (_err, msg) => {
+  console.log(msg);
+  if (msg === 'KILL_SERVER') {
+    client.unsubscribe();
+    client.quit();
+  }
 });
